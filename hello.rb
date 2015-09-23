e97ceb3e8d1655e6cebaefcc9d485ff0be607f9c
None <a1502ma@aiit.ac.jp>
2015-09-23 06:57:13 +0000

    add link to win a car

--- hello.rb
+++ hello.rb
@@ -1,16 +1,18 @@
 require 'sinatra'
 
 get '/' do
-  "Hello AIIT!"
+  "Hello AIIT!<br>" +
+  "<a href='win_a_car'>Win a Car</a>"
 end
 
-# win a car 
+# win a car
 set(:probability) { |value| condition { rand <= value } }
 get '/win_a_car', :probability => 0.3 do
-  "win!!"
+  "win!!<br>" +
+  "<a href='..'>Back</a>"
 end
 
 get '/win_a_car' do
-  "loose!"
+  "loose!<br>" +
+  "<a href='..'>Back</a>"
 end
-
