ab711e15e1599505a5b4db5f7eea0f687cabc9b0
kinchan33 <a1520mk@aiit.ac.jp>
2015-09-23 06:31:38 +0000

    Add win a car program.

--- hello.rb
+++ hello.rb
@@ -3,3 +3,14 @@ require 'sinatra'
 get '/' do
   "Hello World!"
 end
+
+# win a car 
+set(:probability) { |value| condition { rand <= value } }
+get '/win_a_car', :probability => 0.3 do
+  "win!!"
+end
+
+get '/win_a_car' do
+  "loose!"
+end
+
